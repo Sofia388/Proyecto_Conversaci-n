a14fe5b0fc7307bdaa437bcb173371063021796c
ethan10rodriguez <62311552+ethan10rodriguez@users.noreply.github.com>
2021-08-11 08:14:40 -0600

    Add files via upload

--- Autoload.php
+++ Autoload.php
@@ -1,17 +1,17 @@
 <?php
          function Autoload($class)
          {
-             if(is_file('Controller/'.$class.'.Controller.php'))
+             if(is_file('Controllers/'.$class.'.Controller.php'))
              {
-               require_once('Controller/'.$class.'.Controller.php');
+               require_once('Controllers/'.$class.'.Controller.php');
              }
              else if(is_file('Framework/Smarty/libs/'.$class.'.class.php'))
              {
                require_once('Framework/Smarty/libs/'.$class.'.class.php');
              }
-             else if (is_file('Model/'.$class.'.Model.php'))
+             else if (is_file('Models/'.$class.'.Model.php'))
              {
-               require_once('Model/'.$class.'.Model.php');
+               require_once('Models/'.$class.'.Model.php');
 
              }
          }
