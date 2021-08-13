272b87f18667f2b3f50546aef9141e97a3a6e0b3
ethan10rodriguez <62311552+ethan10rodriguez@users.noreply.github.com>
2021-08-12 18:57:05 -0600

    index

---
+++ Index.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+    require_once ('Autoload.php');
+
+    if(isset($_GET['controllers']) && isset($_GET['method']))
+    {
+        $controller=$_GET['controllers'];
+        $method=$_GET['method'];
+    }
+    else
+    {
+       $controller="Home";
+       $method="Inicio";
+    }
+
+    if(class_exists($controller) && method_exists($controller,$method))
+    {
+
+        $class=new $controller;
+        $class->$method();
+
+    }
+    else
+    {
+        echo "clase no existe o el metodo es incorrecto";
+    }
+?>
